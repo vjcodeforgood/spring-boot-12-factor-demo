4a42fc76a81bd2406d125d8cf34f956c1a6bbb62
Vijayakumar <vijay.manirathnam@gmail.com>
2024-10-03 16:48:09 +0530

    first commit

---
+++ src/main/java/com/example/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Value("${app.message}")
+    private String message;
+
+    @GetMapping("/")
+    public String hello() {
+        return message;
+    }
+}
